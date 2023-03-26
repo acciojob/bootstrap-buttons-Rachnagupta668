434bea69d239b8436873480f242203af39bbe789
Rachna <106364652+Rachnagupta668@users.noreply.github.com>
2023-03-26 18:48:37 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,2 @@
 //your code here
-https://d3dyfaf3iutrxo.cloudfront.net/thumbnail/assignment/question/2c53c2c7e03f48a7903dc7591fffe196.png
+// https://d3dyfaf3iutrxo.cloudfront.net/thumbnail/assignment/question/2c53c2c7e03f48a7903dc7591fffe196.png
